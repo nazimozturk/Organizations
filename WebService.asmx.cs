fc3637c300ac6ddc83b37830e5cccbb3fab1dca6
Nazım ÖZTÜRK <Nazm.OZTURK@hotmail.com.tr>
2022-04-28 15:10:51 +0300

    Add project files.

---
+++ WebService.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Organizations
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string Alms()
+        {
+            using (SqlConnection connection = new SqlConnection("Server = .; Database = Alms; Trusted_Connectin = True"))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM ALMS FOR JSON AUTO");
+                connection.Close();
+            };
+            return "a";
+        }
+    }
+}
